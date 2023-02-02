117d1a28b2ee4cca2d95f0c992460f2050f6d278
pAoOAk14pG <bFa2xRMZ@outlook.com>
2023-02-02 19:59:32 +0800

    Added comments to make the code more readable

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Piece numbers associated with ID in HTML file
+// Piece numbers associated with ID in HTML file. Array used for most game logic
 let boardState = [
   null, 63, null, 64, null, 65, null, 66,
   67, null, 68, null, 69, null, 70, null,
@@ -15,8 +15,8 @@ let pieceID;
 let pieceIndex;
 let redPiecesLeft = 12;
 let blackPiecesLeft = 12;
-let capturePieceIndex = [-1. -1, -1, -1];
-let captureMovementIndex = [-1, -1, -1, -1];
+let capturePieceIndex = [-1. -1, -1, -1]; //Marks the indexes of pieces that can be captured by the last selected piece. One index for each direction to move in
+let captureMovementIndex = [-1, -1, -1, -1];//Marks the indexes of checker squares that can be moved to when a capture is made. Indexes align with above array
 
 function clearCapturePieceArrays(){
   capturePieceIndex = [-1. -1, -1, -1];
@@ -63,23 +63,23 @@ function analyzeBoardState(){
 }
 
 const manageCheckers = (e) => {
-  if ($(e.target).hasClass("selected-checker") === true){;
+  if ($(e.target).hasClass("selected-checker") === true){; //Is the clicked square a legal move for the last selected piece? If so, update the boardState array and the HTML file to reflect the changes
     let moveCheckerID = parseInt($(e.target).attr("id"));
     boardState[moveCheckerID] = pieceID;
     $("#" + pieceID.toString()).appendTo($(e.target));
     boardState[pieceIndex] = null;
     $("#" + pieceIndex.toString()).empty();
-    if (captureMovementIndex.includes(moveCheckerID) === true){
+    if (captureMovementIndex.includes(moveCheckerID) === true){ //Was the last move made a capture? If so, remove the captured piece in the boardState array and the HTML file
       let removePieceID = capturePieceIndex[captureMovementIndex.indexOf(moveCheckerID)];
       boardState[removePieceID] = null;
       $("#" + removePieceID.toString()).empty();
       if (turn === false){blackPiecesLeft--;}
       else{redPiecesLeft--;}
     }
-    if (turn === false && moveCheckerID >= 56){
+    if (turn === false && moveCheckerID >= 56){ //Is it red's turn, and if so has the last moved piece been moved to a back row checker square?
       $("#" + pieceID.toString()).addClass("king-piece");
     }
-    if (turn === true && moveCheckerID <= 7){
+    if (turn === true && moveCheckerID <= 7){ //Is it black's turn, and if so has the last moved piece been moved to a back row checker square?
       $("#" + pieceID.toString()).addClass("king-piece");
     }
     turn = Boolean(turn ^ 1) //Bitwise XOR used as hacky way to toggle a boolean
@@ -91,18 +91,17 @@ const manageCheckers = (e) => {
 }
 
 function checkLeftUpDiagonal(){
-  if (pieceIndex % 8 != 0 && pieceIndex >= 10){
-    if (pieceIndex % 8 != 1 && pieceIndex >= 19){
-      if (turn === false && boardState[pieceIndex - 9] >= 75){
-        if (boardState[pieceIndex - 18] === null){
+  if (pieceIndex % 8 != 0 && pieceIndex >= 10){ //Is the last selected piece NOT on the left edge of the board AND is it located far forward enough that it can't move up to the left?
+    if (pieceIndex % 8 != 1 && pieceIndex >= 19){ //Is the last selected piece NOT on the second column AND is it located far forward enough that it can't capture up to the left?
+      if (turn === false && boardState[pieceIndex - 9] >= 75){ //Is it red's turn, AND is the piece to the last selected piece's top left a black piece?
+        if (boardState[pieceIndex - 18] === null){ //Is the checker square two times up to the left empty?
           capturePieceIndex[0] = pieceIndex - 9;
           captureMovementIndex[0] = pieceIndex - 18;
           markChecker(pieceIndex - 18);
         }
       }
-      if (turn === true && boardState[pieceIndex - 9] <= 74){
-        if (boardState[pieceIndex - 18] === null && boardState[pieceIndex - 9] >= 63){
-            capturePieceIndex[0] = pieceIndex - 9;
+      if (turn === true && boardState[pieceIndex - 9] <= 74){ //Is it black's turn? (Also one half of a check if the piece to the top left of the last selected is red)
+        if (boardState[pieceIndex - 18] === null && boardState[pieceIndex - 9] >= 63){ //Is the square two times up to the left empty? (Also the other half of the aforementioned red piece check)
             captureMovementIndex[0] = pieceIndex - 18;
             markChecker(pieceIndex - 18);
         }
@@ -112,7 +111,7 @@ function checkLeftUpDiagonal(){
       markChecker(pieceIndex - 9);
     }
   }
-  return 0;
+  return 0; //The other three functions have similar formats, just with different magic numbers to identify a different direction
 }
 
 function checkRightUpDiagonal(){
@@ -193,7 +192,7 @@ function checkRightDownDiagonal(){
 function handleMovementValidity(){
   clearCapturePieceArrays();
   removeMarkedCheckers();
-  if (pieceID <= 74 && turn === false){
+  if (pieceID <= 74 && turn === false){ //Was the last selected piece a red piece AND is it red's turn? (Prevents black pieces from being moved)
     if ($("#" + pieceID.toString()).hasClass("king-piece") === true){
       checkLeftUpDiagonal();
       checkRightUpDiagonal();
@@ -201,7 +200,7 @@ function handleMovementValidity(){
     checkLeftDownDiagonal();
     checkRightDownDiagonal();
   }
-  else if (pieceID >= 75 && turn === true){
+  else if (pieceID >= 75 && turn === true){ //Was the last selected piece a black piece AND is it blacks's turn? (Prevents red pieces from being moved)
     if ($("#" + pieceID.toString()).hasClass("king-piece") === true){
       checkLeftDownDiagonal();
       checkRightDownDiagonal();
